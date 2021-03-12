fdfd60bbc720893af68ced0f0dd4ce41914ac437
Andrii Shema <andshema@gmail.com>
2021-03-12 16:02:04 +0200

    Добавьте файлы проекта.

---
+++ ImageTest/Person.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageTest
+{
+    static class Person
+    {
+        // a method that calculates the total time for which the work will be done. ( A=V*t ). t=A/(V1+V2+V3) 
+        public static double TotalTime(int[] items, int countImg)
+        {
+            double sum = 0;
+            for (int i = 0; i < items.Length; i++)
+            {
+                sum += (double)1 / items[i];
+
+            }
+            return Math.Ceiling(countImg / sum);
+        }
+
+        // a method that determines whether a number is prime
+        private static bool IsPrimeNumber(int number)
+        {
+            int sqrtNumber = (int)(Math.Sqrt(number));
+            for (int i = 2; i <= sqrtNumber; i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+            return true;
+        }
+
+        // a method that counts the work of each employee 
+        public static void Each_Make(double timeTotal, int restImage, int[] items)
+        {
+            //save personal work 
+            int[] personalWork = new int[items.Length];
+
+            // for each employee we check his productivity depending on his speed for the total time 
+            for (int i = 1; i <= timeTotal; i++)
+            {
+                for (int j = 0; j < items.Length; j++)
+                {
+                    if (IsPrimeNumber(i) & (i % items[j] == 0) & restImage != 0)
+                    {
+                        personalWork[j] += 1;
+                        restImage -= 1;
+                    }
+                    else
+                    if ((i % items[j] == 0) & (i != 0) & restImage != 0)
+                    {
+                        personalWork[j] += 1;
+                        restImage -= 1;
+                    }
+                }
+            }
+
+            // show productivity of each employee 
+            for (int i = 0; i < personalWork.Length; i++)
+            {
+                Console.WriteLine("Person {0} edit: {1} images ", i + 1, personalWork[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ImageTest/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImageTest;
+
+namespace ImageTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {      
+            Console.Write("Enter the number of team members: ");          
+            int countCrew = Int32.Parse(Console.ReadLine());
+
+            // save personal speeds of employees 
+            int[] speedPersons = new int[countCrew];
+
+            for (int i = 0; i < speedPersons.Length; i++)
+            {
+                Console.Write("Enter the personal speed of the {0} employee: ", i + 1);              
+                speedPersons[i] = Int32.Parse(Console.ReadLine());
+            }
+            Console.WriteLine();
+
+            // total number of pictures  
+            Console.Write("Enter the number of pictures to edit: ");
+            int allImages = Int32.Parse(Console.ReadLine());
+
+            // total working time 
+            double totalTime = Person.TotalTime(speedPersons, allImages);
+
+            // show total time
+            Console.WriteLine("The total working time is: " + totalTime + " minutes \n");
+
+            // show personal work 
+            Person.Each_Make(totalTime, allImages, speedPersons);
+
+            Console.ReadLine();             
+        }       
+    }
+}
